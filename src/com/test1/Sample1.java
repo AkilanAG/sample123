dc3d7fa7136bc3ada8f9b588fa59cd2a8c24c15b
AkilanAG <akilan231@gmail.com>
2022-08-19 12:38:22 +0530

    mytask1

---
+++ src/com/test1/Sample1.java
@@ -0,0 +1,19 @@
+package com.test1;
+
+
+public class Sample1 {
+	private void add() {
+		System.out.println("add is a new");
+
+	}
+	private void a() {
+		System.out.println("a is a old");
+
+	}
+	public static void main(String[] args) {
+		Sample1 s=new Sample1();
+		s.add();
+		s.a();
+	}
+
+}
