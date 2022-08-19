3077b93d09b7819fa71a028b2067436056449819
AkilanAG <akilan231@gmail.com>
2022-08-19 12:51:07 +0530

    trail1

--- src/com/test1/Sample1.java
+++ src/com/test1/Sample1.java
@@ -4,7 +4,9 @@ package com.test1;
 public class Sample1 {
 	private void add() {
 		System.out.println("add is a new");
-
+	}
+	private void m1() {
+		System.out.println("test1");
 	}
 	private void a() {
 		System.out.println("a is a old");
