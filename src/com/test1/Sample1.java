b698107bde302ace839f7f60d296ed3cb3adc5a8
AkilanAG <akilan231@gmail.com>
2022-08-19 12:54:10 +0530

    conflict1

--- src/com/test1/Sample1.java
+++ src/com/test1/Sample1.java
@@ -4,7 +4,9 @@ package com.test1;
 public class Sample1 {
 	private void add() {
 		System.out.println("add is a new");
-
+	}
+	private void m2() {
+		System.out.println("test2");
 	}
 	private void a() {
 		System.out.println("a is a old");
